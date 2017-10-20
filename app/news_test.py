91c1698fd20e9a18efec2bf0169a5ba5781536f4
Neville Oronni <nevooronni@gmail.com>
2017-10-20 16:22:04 +0300

    added class objects for our new source instance and run 2 tests on them

---
+++ app/news_test.py
@@ -0,0 +1,30 @@
+import unittest 
+from models import news
+News = news.News
+
+class NewsTest(unittest.TestCase):
+	"""
+	Test case to test the behaviour of the news class
+	"""
+
+	def setUp(self):
+		"""
+		set up method that will run before every test
+		"""
+		self.new_news_source = News("al-jazeera-english","Al Jazeera English","News, analysis from the Middle East and worldwide, multimedia and interactives, opinions, documentaries, podcasts, long reads and broadcast schedule.","http://www.aljazeera.com","general","en","us")
+
+	def test_instance(self):
+		self.assertTrue(isinstance(self.new_news_source,News))
+
+	def test_init(self):
+		"""
+		test_init test case to test if the object is initialized porperly
+		"""
+
+		self.assertEqual(self.new_news_source.id,"al-jazeera-english")
+		self.assertEqual(self.new_news_source.name,"Al Jazeera English")
+		self.assertEqual(self.new_news_source.description,"News, analysis from the Middle East and worldwide, multimedia and interactives, opinions, documentaries, podcasts, long reads and broadcast schedule.")
+		self.assertEqual(self.new_news_source.url,"http://www.aljazeera.com")
+
+if __name__ == '__main__':
+  unittest.main()
--- app/request.py
+++ app/request.py
@@ -1,4 +1,11 @@
 from app import app
+from urllib.request,json#module help us create a connection to our api url and json module will format the json response to a python dictonary
+from .models import news#from package models import module news and select class news\
+
+News = news.News
 
 #Getting api key
 api_key = app.congig['NEWS_API_KEY']
+
+#Getting the news base url
+base_url = app.config['MOVIE_API_BASE_URL']
