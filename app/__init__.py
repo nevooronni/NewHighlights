3e6af1c11400f04a1e7709f451a71a5e63109b5b
Neville Oronni <nevooronni@gmail.com>
2017-10-21 20:25:09 +0300

    added bootrap and initialized it and created custom bootstrap template that can be inherited by child templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from .config import DevConfig
 
 #initializing the applictaion 
@@ -7,5 +8,8 @@ app = Flask(__name__,instance_relative_config = True)#when the instance is creat
 #setting up configuration 
 app.config.from_object(DevConfig)#import the devconfig sublass to use in our app  and use the app.config.from_object
 app.config.from_pyfile('config.py')#connects to config.py all all of its contencts appended to app.config
+
+#initialize flask extensions
+bootstrap = Bootstrap(app)#we initialize the bootrap class by passing in the app instance hwo most extensions are initialized in flask
 
 from app import news
