e3781b2e24486f4e2934a7cbcb0f7c5099f13bec
Neville Oronni <nevooronni@gmail.com>
2017-10-22 21:53:45 +0300

    added custom css file

--- app/error.py
+++ app/error.py
@@ -3,7 +3,7 @@ from app import app
 
 @app.errorhandler(404)
 def four_Ow_four(error):
-	"""
-	function that renders the 404 error page
-	"""
-	return render_template('fourOwfour.html'),404
+    '''
+    Function to render the 404 error page
+    '''
+    return render_template('fourOwfour.html',404)
